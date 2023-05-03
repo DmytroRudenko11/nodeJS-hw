9cdeb5e82483d44c714756b83806bb83b1e2cbff
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2023-05-03 15:37:42 +0300

    method patch added, model contact created, reorganized validation schemas

--- models/contact.js
+++ models/contact.js
@@ -1,11 +1,33 @@
 const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../helpers");
 
-const contactSchema = new Schema({
-  name: String,
-  email: String,
-  phone: String,
-  favourite: Boolean,
-});
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      match: [
+        /^\d{3}-\d{3}-\d{3}$/,
+        "phone number should be in `xxx-xxx-xxx` format",
+      ],
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", handleMongooseError);
 
 const Contact = model("contact", contactSchema);
 
